1ada48f6f9da9f0b5f2f33073374cd367d505db8
intantunti15 <127914172+intantunti15@users.noreply.github.com>
2024-04-05 19:28:32 +0800

    Add files via upload

---
+++ luassegitiga.java
@@ -0,0 +1,29 @@
+
+package tugas1;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class luassegitiga {
+    public static void main(String args[]){
+           
+    Scanner input = new Scanner(System.in).useLocale(Locale.US);
+      
+    System.out.println("##  Program Java Menghitung Luas Segitiga  ##");
+    System.out.println("=============================================");
+    System.out.println();
+     
+    double a,t, luas;
+ 
+    System.out.print("Input alas segitiga: ");
+    a = input.nextDouble();
+      
+    System.out.print("Input tinggi segitiga: ");
+    t = input.nextDouble();
+  
+    luas = 0.5 * a * t;
+      
+    System.out.println("Luas segitiga: "+luas);
+ 
+  }
+}
